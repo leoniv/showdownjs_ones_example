c1599503a198a13adffff15d681bbb6564806054
Leonid Vlasov <leoniv.vlasov@gmail.com>
2017-01-17 12:43:58 +0700

    rm OLE dependency

--- Rakefile
+++ Rakefile
@@ -1,9 +1,14 @@
-require "rake/testtask"
+#require "rake/testtask"
 
-Rake::TestTask.new(:test) do |t|
-  t.libs << "test"
-  t.libs << "lib"
-  t.test_files = FileList['test/**/*_test.rb']
+#Rake::TestTask.new(:test) do |t|
+#  t.libs << "test"
+#  t.test_files = FileList['test/test_helper.rb']
+#end
+
+$LOAD_PATH.unshift File.expand_path('../test', __FILE__)
+require 'test_helper'
+task :run do |t|
+  ShowdownjsOnesExampleTest.new.run
 end
 
-task :default => :test
+task :default => :run
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -1,7 +1,7 @@
-require 'ass_tests/minitest'
+require 'ass_tests'
 
 
-module ShowdownjsOnesExampleTest
+class ShowdownjsOnesExampleTest
   module Fixt
     TMP_DIR = File.expand_path('../../tmp', __FILE__)
     FileUtils.mkdir_p TMP_DIR
@@ -18,13 +18,50 @@ module ShowdownjsOnesExampleTest
 
   IB = AssTests::InfoBases[:showdownjs_ones_example]
 
-  module Runtimes
-    module Thin
-      is_ole_runtime :thin
+  IB.rebuild! :yes
 
-      run IB
+  def wait
+    print 'Press Ctrl-C or close designer for exit from example. '
+    designer.run.wait
+  end
+
+  def self.enterprise
+    @enterprise ||= IB.enterprise :thin do
+      dEbug
+      debuggerUrl 'tcp://localhost'
     end
   end
-end
 
-require 'ass_tests/minitest/autorun'
+  def self.designer
+    @designer ||= IB.designer
+  end
+
+  def designer
+    self.class.designer
+  end
+
+  def enterprise
+    self.class.enterprise
+  end
+
+  def thread
+    @thread ||= Thread.current
+  end
+
+  trap 'SIGINT' do
+    puts 'Bye ;)'
+    exit!
+  end
+
+  at_exit do
+    puts 'Bye ;)'
+  end
+
+  def run
+    thread
+    designer.run
+    enterprise.run
+    wait
+    enterprise.process_holder.kill
+  end
+end
